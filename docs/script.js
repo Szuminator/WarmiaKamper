658cb2343caa70809d04ef94a21e54c8dc17b0a9
Adam Szumacher <78536279+Szuminator@users.noreply.github.com>
2026-05-12 16:27:15 +0200

    Add files via upload

--- docs/script.js
+++ docs/script.js
@@ -45,11 +45,3 @@ const navLinks = document.querySelector('.nav-links');
 menuToggle.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 });
-
-document.querySelectorAll('.nav-links a').forEach(link => {
-
-  link.addEventListener('click', () => {
-    navLinks.classList.remove('active');
-  });
-
-});
